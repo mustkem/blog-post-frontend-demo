dfda2bffe13eea5b0c337c8aa1e89daefd3103aa
mustkem <mo.mustkem@gmail.com>
2020-12-12 02:22:52 +0530

    add

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 // export const API_PATH = "http://localhost:8080";
-export const API_PATH = "http://blog-post.us-east-2.elasticbeanstalk.com/";
+// export const API_PATH = "http://blog-post.us-east-2.elasticbeanstalk.com/";
+export const API_PATH = 'https://blog-post-3.herokuapp.com';
+
